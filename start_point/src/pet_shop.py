72c31aadc203a8b67dcea1621f4b56a460919b3e
Katie Strong <katiestrong123@gmail.com>
2020-11-08 19:06:56 +0000

    Integration testing started.

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -24,42 +24,59 @@ def get_pets_by_breed(list, breed):
             pets_by_breed.append(pet)
     return pets_by_breed
 
-def find_pet_by_name(list, name):
+def find_pet_by_name(list, pet_name):
     for pet in list["pets"]:
-        if pet["name"] == name:
+        if pet["name"] == pet_name:
             return pet
 
-def remove_pet_by_name(list, name):
+def remove_pet_by_name(list, pet_name):
     for pet in list["pets"]:
-        if  pet["name"] == name:
+        if pet["name"] == pet_name:
             list["pets"].remove(pet)
 
 def add_pet_to_stock(list, pet):
     list["pets"].append(pet)
 
-def get_customer_cash(customer_list):
-    return customer_list["cash"]
+def get_customer_cash(list):
+    return list["cash"]
 
-def remove_customer_cash(customer_list, cash):
-    customer_list["cash"] -= cash
+def remove_customer_cash(list, cash):
+    list["cash"] -= cash
 
-def get_customer_pet_count(customer_list):
-    return len(customer_list["pets"])
+def get_customer_pet_count(list):
+    return len(list["pets"])
 
-def add_pet_to_customer(customer_list, new_pet):
-    customer_list["pets"].append(new_pet)
-    return get_customer_pet_count(customer_list)
+def add_pet_to_customer(list, new_pet):
+    list["pets"].append(new_pet)
+    return get_customer_pet_count(list)
 
 #  # --- OPTIONAL ---
 
-def customer_can_afford_pet(customer_list, price):
-    # can_by_pet = False 
-    customer_cash = customer_list["cash"] 
+def customer_can_afford_pet(list, price):
+    customer_cash = list["cash"] 
     if customer_cash >= 100:
         can_by_pet = True 
     else:
         can_by_pet = False 
     return can_by_pet
 
+ #  # --- INTERGRATION TESTS -----
+   
 
-         
+
+#    CODE 
+            # DOESN'T 
+                    #  WORK 
+def sell_pet_to_customer(list, pet_name, customer_name):
+    if pet_name == find_pet_by_name(list, pet_name):
+        get_customer_pet_count(list) + 1 
+        get_pets_sold(list) + 1 
+        remove_pet_by_name(list, pet_name)
+        add_or_remove_cash(list, 900)
+        remove_customer_cash(list, cash) 
+        
+        
+        
+
+
+# If pet_name == a pet in the pet store and Remove the pet from pet store by name. then take the moeny from the customer, decrement their cash, increment pet store cash. Increment pet store sold pets by one and increment customer pets [] by one. 
